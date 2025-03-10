ad4084edb725f247f0b5dfffb39a2e3d8a35cfe6
nisa2711 <hennisalaia@gmail.com>
2025-03-10 11:47:08 +0700

    Create mahasiswa.php
    buat modul mahasiswa

---
+++ mahasiswa.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Mahasiswa</title>
+</head>
+<body>
+    <h6>Data Mahasiswa AMP</h6>
+    <hr>
+    <h6>NIM : 111111</h6></h6>
+    <h6>NAMA : hennisa</h6></h6>
+    <a href="index.php">kembali</a>
+</body>
+</html>
